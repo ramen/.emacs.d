c6764ebb80f0a386e675d2700f882e4b4f229739
ramen <dave@ramenlabs.com>
2010-09-16 12:26:29 -0700

    Make magit diff background less intense

--- ramen/init-colors.el
+++ ramen/init-colors.el
@@ -1,3 +1,8 @@
 (color-theme-tango-2)
+
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "gray10")
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (set-face-background 'magit-item-highlight "gray20")))
