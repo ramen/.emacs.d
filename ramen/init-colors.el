a23de44f3d0a778f85375a9dba1ed94236fe9aa6
ramen <dave@ramenlabs.com>
2010-09-17 10:00:09 -0700

    ERC customizations

--- ramen/init-colors.el
+++ ramen/init-colors.el
@@ -5,6 +5,10 @@
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "gray10")
 
+(add-hook 'erc-mode-hook
+          '(lambda ()
+             (set-face-foreground 'erc-nick-default-face "#729fcf")))
+
 (add-hook 'magit-mode-hook
           (lambda ()
             (set-face-background 'magit-item-highlight "gray20")))
---
+++ ramen/init-erc.el
@@ -0,0 +1,4 @@
+;;; init-erc.el --- ERC customizations
+
+(set-variable 'erc-nick "ramenboy")
+(set-variable 'erc-nick-uniquifier "_")
