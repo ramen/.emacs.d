cc0c56095f7c625d96bc2df2921a8e0ee68aa98d
ramen <dave@ramenlabs.com>
2010-09-17 09:10:19 -0700

    Dired mode customizations

---
+++ ramen/init-dired.el
@@ -0,0 +1,10 @@
+;;; init-dired.el - Dired mode customizations
+
+(add-hook 'dired-mode-hook
+          '(lambda ()
+             (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)))
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+(setq dired-recursive-copies 'top)
+(setq dired-recursive-deletes 'top)
