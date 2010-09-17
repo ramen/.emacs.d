11fe52a3c2fa2b47da9f14166c2a14ee79583b46
ramen <dave@ramenlabs.com>
2010-09-17 09:01:45 -0700

    Disable auto save

--- ramen/init-misc.el
+++ ramen/init-misc.el
@@ -4,7 +4,8 @@
 (delete-selection-mode 1)
 (winner-mode 1)
 
-(setq initial-scratch-message nil
+(setq auto-save-default nil
+      initial-scratch-message nil
       line-move-visual nil
       mode-line-in-non-selected-windows nil
       parens-require-spaces nil
