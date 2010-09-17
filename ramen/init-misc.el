3a921a38dd5e7aa2c2d2b34f70fe6412306b51a5
ramen <dave@ramenlabs.com>
2010-09-16 17:44:13 -0700

    Mouse settings and bindings

--- ramen/init-misc.el
+++ ramen/init-misc.el
@@ -1,11 +1,10 @@
 (blink-cursor-mode 1)
 (delete-selection-mode 1)
+(winner-mode 1)
 
 (setq initial-scratch-message nil
       line-move-visual nil
       mode-line-in-non-selected-windows nil
-      mouse-wheel-progressive-speed nil
-      mouse-wheel-scroll-amount '(1)
       parens-require-spaces nil
       ring-bell-function 'ignore
       scroll-preserve-screen-position t
---
+++ ramen/init-mouse.el
@@ -0,0 +1,13 @@
+(setq mouse-wheel-progressive-speed nil
+      mouse-wheel-scroll-amount '(1))
+
+;; ctrl-wheel to zoom font size
+(global-set-key [C-mouse-4] 'text-scale-increase)
+(global-set-key [C-mouse-5] 'text-scale-decrease)
+
+;; MX Revolution mouse features
+(global-set-key [mouse-8] 'winner-undo)
+(global-set-key [mouse-9] 'winner-redo)
+(global-set-key [mouse-13] 'bs-cycle-previous)
+(global-set-key [mouse-15] 'bs-cycle-next)
+(global-set-key [mouse-17] 'last-buffer)
