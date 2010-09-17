43a2d1110c0b8f9dfddaa7bbc246c732b3b975ae
ramen <dave@ramenlabs.com>
2010-09-17 14:44:15 -0700

    Turn of hl-line-mode in ERC

--- ramen/init-colors.el
+++ ramen/init-colors.el
@@ -7,6 +7,8 @@
 
 (add-hook 'erc-mode-hook
           (lambda ()
+            (make-local-variable 'global-hl-line-mode)
+            (setq global-hl-line-mode nil)
             (set-face-foreground 'erc-input-face "#fcaf3e")
             (set-face-foreground 'erc-nick-default-face "#729fcf")
             (set-face-foreground 'erc-timestamp-face "gray30")))
