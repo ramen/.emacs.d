138f1b72079b5327a6f8fec342feb5405c5f74df
ramen <dave@ramenlabs.com>
2010-09-20 15:13:28 -0700

    More ERC tweaks

--- ramen/init-erc.el
+++ ramen/init-erc.el
@@ -1,7 +1,9 @@
 ;;; init-erc.el --- ERC customizations
 
-(setq erc-nick "ramenboy"
+(setq erc-email-userid "ramen"
+      erc-format-nick-function 'erc-format-@nick
+      erc-nick "ramenboy"
       erc-nick-uniquifier "_"
       erc-notice-prefix "# "
-      erc-prompt (lambda ()
-                   (concat "[" (or (erc-default-target) "ERC") "]")))
+      erc-prompt (lambda () (concat "[" (or (erc-default-target) "ERC") "]"))
+      erc-user-full-name "Dave Benjamin")
