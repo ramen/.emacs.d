c53204458d055f7fe37f0075a7d75785fb722a00
ramen <dave@ramenlabs.com>
2010-09-19 21:23:00 -0700

    Fonts for various operating systems

---
+++ ramen/init-fonts.el
@@ -0,0 +1,9 @@
+;;; init-fonts.el --- Font configuration
+
+(when window-system
+  (let ((font
+         (case window-system
+           (w32 "fixedsys")
+           (mac "-apple-monaco-medium-r-normal--14-140-72-72-m-140-iso10646-1")
+           (otherwise "-bitstream-Bitstream Vera Sans Mono-bold-normal-normal-*-14-*-*-*-m-0-iso10646-1"))))
+    (set-face-attribute 'default nil :font font)))
