e9ded8424a04e5b89f95455923d79ae2eb8f05de
ramen <dave@ramenlabs.com>
2010-09-20 15:13:52 -0700

    c-mode settings

---
+++ ramen/init-c-mode.el
@@ -0,0 +1,13 @@
+;;; init-c-mode.el - c-mode settings
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (c-set-style "java")
+            (c-set-offset 'case-label '+)
+            (c-set-offset 'substatement-open 0)
+            (setq c-basic-offset tab-width)
+            (setq comment-start "// ")
+            (setq comment-end "")
+            (setq comment-start-skip "// *")
+            (cond ((fboundp 'subword-mode) (subword-mode 1))
+                  ((fboundp 'c-subword-mode) (c-subword-mode 1)))))
