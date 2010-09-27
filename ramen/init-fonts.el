18151b7c9ba794c7934dcab621de83ad97a3ac2d
ramen <dave@ramenlabs.com>
2010-09-27 10:28:09 -0700

    Use DejaVu instead of Bitstream Vera

--- ramen/init-fonts.el
+++ ramen/init-fonts.el
@@ -5,5 +5,5 @@
          (case window-system
            (w32 "fixedsys")
            (mac "-apple-monaco-medium-r-normal--14-140-72-72-m-140-iso10646-1")
-           (otherwise "-bitstream-Bitstream Vera Sans Mono-bold-normal-normal-*-14-*-*-*-m-0-iso10646-1"))))
+           (otherwise "-unknown-DejaVu Sans Mono-bold-normal-normal-*-14-*-*-*-m-0-iso10646-1"))))
     (set-face-attribute 'default nil :font font)))
