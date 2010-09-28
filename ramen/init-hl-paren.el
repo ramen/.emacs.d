bdcf6dcf3baf571f2f5d3195d758ee1a31cecc2c
ramen <dave@ramenlabs.com>
2010-09-27 18:49:57 -0700

    Don't use highlight-parentheses-mode in terminals

--- ramen/init-hl-paren.el
+++ ramen/init-hl-paren.el
@@ -1,8 +1,9 @@
 ;;; init-hl-paren.el --- Enable highlight-parentheses mode
 
-(add-hook 'coding-hook 'highlight-parentheses-mode)
+(when window-system
+  (add-hook 'coding-hook 'highlight-parentheses-mode)
 
-;; http://stackoverflow.com/questions/2413047/ddg#2413472
-(setq hl-paren-colors
-      '("orange1" "yellow1" "greenyellow" "green1"
-        "springgreen1" "cyan1" "slateblue1" "magenta1" "purple"))
+  ;; http://stackoverflow.com/questions/2413047/ddg#2413472
+  (setq hl-paren-colors
+        '("orange1" "yellow1" "greenyellow" "green1"
+          "springgreen1" "cyan1" "slateblue1" "magenta1" "purple")))
