3033e4c4444a117e5eb2ea17e71feacaba9b1d85
ramen <dave@ramenlabs.com>
2010-10-19 17:41:39 -0700

    Shortcut for find-dired that hides boring files, a la M-x rgrep

--- ramen/init-dired.el
+++ ramen/init-dired.el
@@ -8,3 +8,47 @@
 
 (setq dired-recursive-copies 'top)
 (setq dired-recursive-deletes 'top)
+
+(defun rdired (dir)
+  (interactive "DBase directory: ")
+  (require 'grep)
+  (find-dired dir
+              ;; Ripped from rgrep
+              (concat
+               (and grep-find-ignored-directories
+                    (concat (shell-quote-argument "(")
+                            ;; we should use shell-quote-argument here
+                            " -path "
+                            (mapconcat
+                             #'(lambda (ignore)
+                                 (cond ((stringp ignore)
+                                        (shell-quote-argument
+                                         (concat "*/" ignore)))
+                                       ((consp ignore)
+                                        (and (funcall (car ignore) dir)
+                                             (shell-quote-argument
+                                              (concat "*/"
+                                                      (cdr ignore)))))))
+                             grep-find-ignored-directories
+                             " -o -path ")
+                            " "
+                            (shell-quote-argument ")")
+                            " -prune -o "))
+               (and grep-find-ignored-files
+                    (concat (shell-quote-argument "(")
+                            ;; we should use shell-quote-argument here
+                            " -name "
+                            (mapconcat
+                             #'(lambda (ignore)
+                                 (cond ((stringp ignore)
+                                        (shell-quote-argument ignore))
+                                       ((consp ignore)
+                                        (and (funcall (car ignore) dir)
+                                             (shell-quote-argument
+                                              (cdr ignore))))))
+                             grep-find-ignored-files
+                             " -o -name ")
+                            " "
+                            (shell-quote-argument ")")
+                            " -prune -o "))
+               "-type f")))
