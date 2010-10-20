f1a93b6619188a747b5563ea3c51592b740c83fd
ramen <dave@ramenlabs.com>
2010-10-19 21:58:53 -0700

    Remove extra parentheses from find command causing incorrect logic

--- ramen/init-dired.el
+++ ramen/init-dired.el
@@ -12,43 +12,47 @@
 (defun rdired (dir)
   (interactive "DBase directory: ")
   (require 'grep)
-  (find-dired dir
-              ;; Ripped from rgrep
-              (concat
-               (and grep-find-ignored-directories
-                    (concat (shell-quote-argument "(")
-                            ;; we should use shell-quote-argument here
-                            " -path "
-                            (mapconcat
-                             #'(lambda (ignore)
-                                 (cond ((stringp ignore)
-                                        (shell-quote-argument
-                                         (concat "*/" ignore)))
-                                       ((consp ignore)
-                                        (and (funcall (car ignore) dir)
-                                             (shell-quote-argument
-                                              (concat "*/"
-                                                      (cdr ignore)))))))
-                             grep-find-ignored-directories
-                             " -o -path ")
-                            " "
-                            (shell-quote-argument ")")
-                            " -prune -o "))
-               (and grep-find-ignored-files
-                    (concat (shell-quote-argument "(")
-                            ;; we should use shell-quote-argument here
-                            " -name "
-                            (mapconcat
-                             #'(lambda (ignore)
-                                 (cond ((stringp ignore)
-                                        (shell-quote-argument ignore))
-                                       ((consp ignore)
-                                        (and (funcall (car ignore) dir)
-                                             (shell-quote-argument
-                                              (cdr ignore))))))
-                             grep-find-ignored-files
-                             " -o -name ")
-                            " "
-                            (shell-quote-argument ")")
-                            " -prune -o "))
-               "-type f")))
+  (let ((find-ls-option
+         (cons 
+          ;; Ripped from rgrep
+          (concat
+           (and grep-find-ignored-directories
+                (concat (shell-quote-argument "(")
+                        ;; we should use shell-quote-argument here
+                        " -path "
+                        (mapconcat
+                         #'(lambda (ignore)
+                             (cond ((stringp ignore)
+                                    (shell-quote-argument
+                                     (concat "*/" ignore)))
+                                   ((consp ignore)
+                                    (and (funcall (car ignore) dir)
+                                         (shell-quote-argument
+                                          (concat "*/"
+                                                  (cdr ignore)))))))
+                         grep-find-ignored-directories
+                         " -o -path ")
+                        " "
+                        (shell-quote-argument ")")
+                        " -prune -o "))
+           (and grep-find-ignored-files
+                (concat (shell-quote-argument "(")
+                        ;; we should use shell-quote-argument here
+                        " -name "
+                        (mapconcat
+                         #'(lambda (ignore)
+                             (cond ((stringp ignore)
+                                    (shell-quote-argument ignore))
+                                   ((consp ignore)
+                                    (and (funcall (car ignore) dir)
+                                         (shell-quote-argument
+                                          (cdr ignore))))))
+                         grep-find-ignored-files
+                         " -o -name ")
+                        " "
+                        (shell-quote-argument ")")
+                        " -prune -o "))
+           "-type f "
+           (car find-ls-option))
+          (cdr find-ls-option))))
+    (find-dired dir "")))
