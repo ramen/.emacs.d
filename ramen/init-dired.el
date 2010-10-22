dd3c9ddde49daef4f103aae9f2ea944ebb72e982
ramen <dave@ramenlabs.com>
2010-10-21 22:18:29 -0700

    Disable word-wrap in dired, improve layout of find-dired

--- ramen/init-dired.el
+++ ramen/init-dired.el
@@ -2,9 +2,12 @@
 
 (add-hook 'dired-mode-hook
           (lambda ()
-            (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)))
+            (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
+            (setq truncate-lines t)))
 
 (put 'dired-find-alternate-file 'disabled nil)
 
 (setq dired-recursive-copies 'top)
 (setq dired-recursive-deletes 'top)
+
+(setq find-ls-option '("-ls" . "-dils"))
