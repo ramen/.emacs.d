75250d7b7eabb30d79b75d16a3b6934e0f18ad19
unknown <ramen@.(none)>
2011-02-04 08:36:16 -0700

    Add darkroom-mode

---
+++ ramen/init-darkroom.el
@@ -0,0 +1,6 @@
+;;; init-darkroom.el --- Darkroom mode configuration
+
+(require 'darkroom-mode)
+
+(setq w32-fullscreen-toggletitle-cmd
+      (concat dotfiles-dir "/elpa-to-submit/w32toggletitle.exe"))
