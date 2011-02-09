54b0afc1c80ab85fee8b200a0c7759abd6104122
ramen <dave@ramenlabs.com>
2011-02-08 19:05:54 -0700

    Make mouse bindings work on Windows

--- ramen/init-mouse.el
+++ ramen/init-mouse.el
@@ -5,12 +5,22 @@
       mouse-wheel-scroll-amount '(1))
 
 ;; ctrl-wheel to zoom font size
-(global-set-key [C-mouse-4] 'text-scale-increase)
-(global-set-key [C-mouse-5] 'text-scale-decrease)
+(if (eq window-system 'w32)
+    (progn
+      (global-set-key [C-wheel-up] 'text-scale-increase)
+      (global-set-key [C-wheel-down] 'text-scale-decrease))
+  (global-set-key [C-mouse-4] 'text-scale-increase)
+  (global-set-key [C-mouse-5] 'text-scale-decrease))
+
+;; back and next buttons for winner-mode
+(if (eq window-system 'w32)
+    (progn
+      (global-set-key [mouse-4] 'winner-undo)
+      (global-set-key [mouse-5] 'winner-redo))
+  (global-set-key [mouse-8] 'winner-undo)
+  (global-set-key [mouse-9] 'winner-redo))
 
 ;; MX Revolution mouse features
-(global-set-key [mouse-8] 'winner-undo)
-(global-set-key [mouse-9] 'winner-redo)
 (global-set-key [mouse-13] 'bs-cycle-previous)
 (global-set-key [mouse-15] 'bs-cycle-next)
 (global-set-key [mouse-17] 'last-buffer)
