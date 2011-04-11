e08412585dd4777ff66fd40121b48d5433f54584
ramen <dave@ramenlabs.com>
2011-04-11 16:56:17 -0700

    Use sshx everywhere

--- ramen/init-w32.el
+++ ramen/init-w32.el
@@ -3,5 +3,4 @@
 (when (eq window-system 'w32)
   (unbold-all-faces)
   (add-hook 'after-change-major-mode-hook 'unbold-all-faces)
-  (setq find-program "gfind"
-        tramp-default-method "sshx"))
+  (setq find-program "gfind"))
