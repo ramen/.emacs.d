226cba4469f261ca02f7f63dd1d50b2a2c624155
ramen <dave@ramenlabs.com>
2011-04-14 10:16:18 -0700

    Don't word-wrap rgrep output

--- ramen/init-dired.el
+++ ramen/init-dired.el
@@ -5,6 +5,10 @@
             (define-key dired-mode-map "e" 'wdired-change-to-wdired-mode)
             (setq truncate-lines t)))
 
+(add-hook 'grep-mode-hook
+          (lambda ()
+            (setq truncate-lines t)))
+
 (put 'dired-find-alternate-file 'disabled nil)
 
 (setq dired-recursive-copies 'top)
