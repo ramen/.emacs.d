fad18817080e524fc3375ca30c5a8c590c897158
ramen <dave@ramenlabs.com>
2011-04-15 10:38:53 -0700

    Leggo my digit-argument

---
+++ ramen/init-git.el
@@ -0,0 +1,12 @@
+;;; init-git.el --- Git and Magit customizations
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (define-key magit-mode-map (kbd "M-1") 'digit-argument)
+            (define-key magit-mode-map (kbd "M-2") 'digit-argument)
+            (define-key magit-mode-map (kbd "M-3") 'digit-argument)
+            (define-key magit-mode-map (kbd "M-4") 'digit-argument)
+            (define-key magit-mode-map (kbd "C-1") 'magit-show-level-1-all)
+            (define-key magit-mode-map (kbd "C-2") 'magit-show-level-2-all)
+            (define-key magit-mode-map (kbd "C-3") 'magit-show-level-3-all)
+            (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)))
