1a6157cc4b6e4901291267abc69aa7c053e6ebb5
ramen <dave@ramenlabs.com>
2011-04-18 10:12:36 -0700

    Add ack support

---
+++ ramen/init-ack.el
@@ -0,0 +1,6 @@
+;;; init-ack.el --- ack (grep alternative) support
+
+(autoload 'ack "full-ack" nil t)
+(autoload 'ack-find-file "full-ack" nil t)
+(autoload 'ack-find-same-file "full-ack" nil t)
+(autoload 'ack-same "full-ack" nil t)
