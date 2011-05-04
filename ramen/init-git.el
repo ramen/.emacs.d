a9281e7d89f25288dc097c025ad8ba72f3ba393c
ramen <dave@ramenlabs.com>
2011-05-03 17:44:27 -0700

    Don't run Magit in a split window

--- ramen/init-git.el
+++ ramen/init-git.el
@@ -9,7 +9,8 @@
             (define-key magit-mode-map (kbd "C-1") 'magit-show-level-1-all)
             (define-key magit-mode-map (kbd "C-2") 'magit-show-level-2-all)
             (define-key magit-mode-map (kbd "C-3") 'magit-show-level-3-all)
-            (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)))
+            (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)
+            (delete-other-windows)))
 
 ;; I know there's a keyboard shortcut for this.
 ;; That doesn't mean I have to like it.
