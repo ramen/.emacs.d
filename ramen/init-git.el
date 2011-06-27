b88d4d62ddb75f4c3dcc6d540cd282bea2bdeb09
Dave Benjamin <daveb@twitter.com>
2011-06-27 12:50:31 -0700

    Add switch for push -u

--- ramen/init-git.el
+++ ramen/init-git.el
@@ -10,7 +10,8 @@
             (define-key magit-mode-map (kbd "C-2") 'magit-show-level-2-all)
             (define-key magit-mode-map (kbd "C-3") 'magit-show-level-3-all)
             (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)
-            (delete-other-windows)))
+            (delete-other-windows)
+            (magit-key-mode-insert-switch 'pushing "-u" "Set upstream" "-u")))
 
 ;; I know there's a keyboard shortcut for this.
 ;; That doesn't mean I have to like it.
