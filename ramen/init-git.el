9bc711fceadd1d8b5ce93f0668918c4428f59d97
Dave Benjamin <daveb@twitter.com>
2011-07-20 14:13:50 -0700

    Remove magit full-window hack, which breaks too many things

--- ramen/init-git.el
+++ ramen/init-git.el
@@ -16,8 +16,7 @@
             (define-key magit-mode-map (kbd "C-1") 'magit-show-level-1-all)
             (define-key magit-mode-map (kbd "C-2") 'magit-show-level-2-all)
             (define-key magit-mode-map (kbd "C-3") 'magit-show-level-3-all)
-            (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)
-            (delete-other-windows)))
+            (define-key magit-mode-map (kbd "C-4") 'magit-show-level-4-all)))
 
 (eval-after-load 'magit
   '(magit-key-mode-insert-switch 'pushing "-u" "Set upstream" "-u"))
