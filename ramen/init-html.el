1e518847c71329f1fe7321e6a2700713e82dff87
Dave Benjamin <daveb@twitter.com>
2011-08-30 07:04:49 -0700

    Make html-mode behave like other coding modes

--- ramen/init-html.el
+++ ramen/init-html.el
@@ -1,5 +1,10 @@
 ;;; init-html.el --- HTML mode customizations
 
+(add-hook 'sgml-mode-hook
+          (lambda ()
+            (run-coding-hook)
+            (local-set-key (kbd "RET") nil)))
+
 (eval-after-load 'sgml-mode
   '(progn
      (setcdr (assoc "p" html-tag-alist) nil)
