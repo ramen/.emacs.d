121c435f044798b45d8a66212b26d97956bd48b8
Dave Benjamin <daveb@twitter.com>
2011-10-04 15:20:45 -0700

    Stop clobbering my enter key

--- ramen/init-javascript.el
+++ ramen/init-javascript.el
@@ -2,7 +2,8 @@
 
 (add-hook 'js2-mode-hook
           (lambda ()
-            (run-coding-hook)))
+            (run-coding-hook)
+            (define-key js2-mode-map (kbd "RET") nil)))
 
 (setq js2-enter-indents-newline t)
 (setq js2-mirror-mode nil)
