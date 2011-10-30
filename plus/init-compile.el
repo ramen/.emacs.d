9bdff63601188a5bd5d763fa04ed776e7e0e78dd
Dave Benjamin <daveb@twitter.com>
2011-10-30 08:19:33 -0700

    Add ANSI color in compile output and autoload recompile

---
+++ plus/init-compile.el
@@ -0,0 +1,12 @@
+;;; init-compile.el --- compilation settings
+
+(require 'ansi-color)
+
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(autoload 'recompile "compile")
