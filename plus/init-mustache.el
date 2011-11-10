c24b92c2f50e3b570dac5075079b27425a4c4bd5
Dave Benjamin <daveb@twitter.com>
2011-11-10 09:54:53 -0700

    Enable Zencoding in Mustache templates

--- plus/init-mustache.el
+++ plus/init-mustache.el
@@ -2,6 +2,11 @@
 
 (require 'mustache-mode)
 
+(add-hook 'tpl-mode-hook
+          (lambda ()
+            (run-coding-hook)
+            (zencoding-mode)))
+
 (define-key tpl-mode-map (kbd "C-c /") 'sgml-close-tag)
 (define-key tpl-mode-map (kbd "C-c C-t") 'sgml-tag)
 (define-key tpl-mode-map (kbd "RET") nil)
