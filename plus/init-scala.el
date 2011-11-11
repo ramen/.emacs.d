738a330e96663a590b5078e15498a6cb5972f104
Dave Benjamin <daveb@twitter.com>
2011-11-11 11:44:09 -0700

    Fix broken indentation in Scala blocks

--- plus/init-scala.el
+++ plus/init-scala.el
@@ -11,3 +11,22 @@
               (lambda () (interactive)
                 (setq last-command nil)
                 (newline-and-indent)))))
+
+(defun scala-goto-block-start (block-start)
+  (real-goto-char block-start)
+  (end-of-line)
+  (if (scala-looking-at-backward scala-expr-start-re)
+      (real-goto-char (+ (current-indentation) scala-mode-indent:step 1))
+    (real-goto-char block-start)))
+
+(defadvice scala-indentation-from-following (around scala-indentation-fix-1 activate)
+  "Fix broken indentation within blocks"
+  (flet ((real-goto-char (position) (goto-char position))
+         (goto-char (position) (scala-goto-block-start position)))
+    ad-do-it))
+
+(defadvice scala-indentation-from-block (around scala-indentation-fix-2 activate)
+  "Fix broken indentation within blocks"
+  (flet ((real-goto-char (position) (goto-char position))
+         (goto-char (position) (scala-goto-block-start position)))
+    ad-do-it))
