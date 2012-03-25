8514425f3c7c7d700fbf685136f557bb60f0b040
Dave Benjamin <daveb@twitter.com>
2012-03-25 13:59:20 -0700

    Run sbt compile to compile thrift files

--- plus/init-thrift.el
+++ plus/init-thrift.el
@@ -1,3 +1,10 @@
 ;;; init-thrift.el --- Thrift mode configuration
 
 (add-to-list 'auto-mode-alist '("\\.thrift\\'" . thrift-mode))
+
+(add-hook 'thrift-mode-hook
+          (lambda ()
+            (run-coding-hook)
+            (set (make-local-variable 'compile-command)
+                 (format "cd %s && sbt compile"
+                         (locate-dominating-file default-directory ".git")))))
