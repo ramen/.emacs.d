14377622a1104aa0604738ca7d7c06c9897ba6c3
Dave Benjamin <daveb@twitter.com>
2012-05-09 22:06:21 -0700

    Update mustache-mode

--- plus/init-mustache.el
+++ plus/init-mustache.el
@@ -2,12 +2,12 @@
 
 (require 'mustache-mode)
 
-(add-hook 'tpl-mode-hook
+(add-hook 'mustache-mode-hook
           (lambda ()
             (run-coding-hook)
             (setq comment-start-skip "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\);+ *")
             (zencoding-mode)))
 
-(define-key tpl-mode-map (kbd "C-c /") 'sgml-close-tag)
-(define-key tpl-mode-map (kbd "C-c C-t") 'sgml-tag)
-(define-key tpl-mode-map (kbd "RET") nil)
+(define-key mustache-mode-map (kbd "C-c /") 'sgml-close-tag)
+(define-key mustache-mode-map (kbd "C-c C-t") 'sgml-tag)
+(define-key mustache-mode-map (kbd "RET") nil)
