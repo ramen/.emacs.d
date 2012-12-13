e108515555c3c55b30efecf20ae72d7692532979
Dave Benjamin <daveb@twitter.com>
2012-12-13 13:24:48 -0700

    Convert "val" to "var" (thanks, Scala)

--- plus/init-javascript.el
+++ plus/init-javascript.el
@@ -3,6 +3,7 @@
 (add-hook 'js2-mode-hook
           (lambda ()
             (run-coding-hook)
+            (abbrev-mode 1)
             (define-key js2-mode-map (kbd "RET") nil)))
 
 (setq js2-enter-indents-newline t)
