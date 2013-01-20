789c09ab375795cbbe1d2d98430d601117d6583b
Dave Benjamin <daveb@twitter.com>
2013-01-20 14:49:47 -0700

    Fix hook to turn on highlight-parentheses-mode

--- personal/init-hl-paren.el
+++ personal/init-hl-paren.el
@@ -1,9 +1,12 @@
 ;;; init-hl-paren.el --- Enable highlight-parentheses mode
 
 (when window-system
-  (add-hook 'prog-mode-hook 'highlight-parentheses-mode)
+  (add-hook 'prog-mode-hook 'turn-on-highlight-parentheses-mode))
 
-  ;; http://stackoverflow.com/questions/2413047/ddg#2413472
-  (setq hl-paren-colors
-        '("orange1" "yellow1" "greenyellow" "green1"
-          "springgreen1" "cyan1" "slateblue1" "magenta1" "purple")))
+(defun turn-on-highlight-parentheses-mode ()
+  (highlight-parentheses-mode t))
+
+;; http://stackoverflow.com/questions/2413047/ddg#2413472
+(setq hl-paren-colors
+      '("orange1" "yellow1" "greenyellow" "green1"
+        "springgreen1" "cyan1" "slateblue1" "magenta1" "purple"))
