82257594ffb795fa0f5dcce677b667d65c856da9
Dave Benjamin <daveb@twitter.com>
2013-02-23 00:31:37 -0700

    Add custom bs-show configuration

---
+++ personal/init-bs.el
@@ -0,0 +1,20 @@
+;;; init-bs.el --- buffer selection mode configuration
+
+(require 'bs)
+
+(let ((name "custom")
+      (must-show-regexp nil)
+      (must-show-function nil)
+      (dont-show-regexp "^\\*\\(Compile-Log\\|Messages\\|scratch\\)\\*$")
+      (dont-show-function (lambda (buf)
+                            (with-current-buffer buf
+                              (eq major-mode 'dired-mode))))
+      (buffer-sort-function 'bs-sort-buffer-interns-are-last))
+  (add-to-list 'bs-configurations
+               (list name
+                     must-show-regexp must-show-function
+                     dont-show-regexp dont-show-function
+                     buffer-sort-function)))
+
+(setq bs-alternative-configuration "files"
+      bs-default-configuration "custom")
