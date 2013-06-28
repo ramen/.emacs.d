cc7e9f8a5f448c29c95448de27716bfcda8cf617
Dave Benjamin <daveb@twitter.com>
2013-06-28 09:47:36 -0700

    Don't mess up colors on grep compilations

--- personal/init-compile.el
+++ personal/init-compile.el
@@ -3,9 +3,10 @@
 (require 'ansi-color)
 
 (defun colorize-compilation-buffer ()
-  (toggle-read-only)
-  (ansi-color-apply-on-region (point-min) (point-max))
-  (toggle-read-only))
+  (unless (eq major-mode 'grep-mode)
+    (toggle-read-only)
+    (ansi-color-apply-on-region (point-min) (point-max))
+    (toggle-read-only)))
 
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
