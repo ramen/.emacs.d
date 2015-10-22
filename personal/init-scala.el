ebb6ae9be5704eb73e4b367461cc6772a084ed82
Dave Benjamin <dave@ramenlabs.com>
2015-10-22 13:33:26 -0700

    Refactor pants compile command

--- personal/init-scala.el
+++ personal/init-scala.el
@@ -12,21 +12,19 @@
                 (setq last-command nil)
                 (newline-and-indent)))
             (define-key scala-mode-map (kbd "C-c C-s") 'scala-run-scala)
-            (set (make-local-variable 'compile-command)
-                 (let ((pants-dir (locate-dominating-file default-directory "BUILD"))
-                       (mvn-dir (locate-dominating-file default-directory "pom.xml"))
-                       (sbt-dir (locate-dominating-file default-directory "project")))
-                   (cond (pants-dir
-                          (let* ((pants-root (locate-dominating-file default-directory "pants"))
-                                 (src-dir (locate-dominating-file default-directory "src"))
-                                 (proj-dirs (nreverse (split-string src-dir "/" t)))
-                                 (subproj (pop proj-dirs))
-                                 (proj (pop proj-dirs)))
-                                 (format "cd %s && ./pants test %s/%s::"
-                                         pants-root
-                                         proj
-                                         subproj)))
-                         (mvn-dir (format "cd %s && mvn test" mvn-dir))
-                         (sbt-dir (format "cd %s && sbt test" sbt-dir)))))
+            (set-pants-compile-command)
             (cond ((fboundp 'subword-mode) (subword-mode 1))
                   ((fboundp 'c-subword-mode) (c-subword-mode 1)))))
+
+(defun set-pants-compile-command ()
+  (set (make-local-variable 'compile-command)
+       (let* ((pants-dir (locate-dominating-file default-directory "BUILD"))
+              (pants-root (locate-dominating-file default-directory "pants"))
+              (src-dir (locate-dominating-file default-directory "src"))
+              (proj-dirs (nreverse (split-string src-dir "/" t)))
+              (subproj (pop proj-dirs))
+              (proj (pop proj-dirs)))
+         (format "cd %s && ./pants test %s/%s::"
+                 pants-root
+                 proj
+                 subproj))))
