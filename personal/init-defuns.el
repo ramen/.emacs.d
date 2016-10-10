b4ae09db284d2985b60839ea56f856e4f7e57243
Dave Benjamin <dave@ramenlabs.com>
2016-10-10 11:11:19 -0700

    URL encoding utilities

--- personal/init-defuns.el
+++ personal/init-defuns.el
@@ -349,6 +349,21 @@ default length of 8 characters."
       (setq last-major-mode major-mode)
       (text-mode))))
 
+;; http://www.blogbyben.com/2010/08/handy-emacs-function-url-decode-region.html
+(defun url-decode-region (start end)
+  "Replace a region with the same contents, only URL decoded."
+  (interactive "r")
+  (let ((text (url-unhex-string (buffer-substring start end) t)))
+    (delete-region start end)
+    (insert text)))
+
+(defun url-encode-region (start end)
+  "Replace a region with the same contents, only URL encoded."
+  (interactive "r")
+  (let ((text (url-hexify-string (buffer-substring start end))))
+    (delete-region start end)
+    (insert text)))
+
 ;; I don't like idle-highlight-mode.
 (remove-hook 'prog-mode-hook 'idle-highlight-mode)
 
