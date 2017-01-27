8b4f90b0fb42b2b90f88219811afe7be6130fa55
Dave Benjamin <dave.benjamin@gmail.com>
2017-01-27 10:08:29 -0700

    Fix font for Windows

--- personal/init-fonts.el
+++ personal/init-fonts.el
@@ -3,7 +3,7 @@
 (when window-system
   (let ((font
          (case window-system
-           (w32 "-outline-Fixedsys Excelsior 3.01-normal-normal-normal-sans-16-*-*-*-c-*-iso10646-1")
+           (w32 "-outline-Consolas-normal-normal-normal-*-14-*-*-*-c-*-iso10646-1")
            (ns "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
            (otherwise "-unknown-DejaVu Sans Mono-bold-normal-normal-*-14-*-*-*-m-0-iso10646-1"))))
     (set-face-attribute 'default nil :font font)))
