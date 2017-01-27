7ccfdb126cd87cc4b7f4ef9e832ac2a1b13f6d15
Dave Benjamin <dave@ramenlabs.com>
2017-01-27 09:37:53 -0700

    Fix fonts on OS X for Emacs 25

--- personal/init-fonts.el
+++ personal/init-fonts.el
@@ -4,6 +4,6 @@
   (let ((font
          (case window-system
            (w32 "-outline-Fixedsys Excelsior 3.01-normal-normal-normal-sans-16-*-*-*-c-*-iso10646-1")
-           (mac "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+           (ns "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
            (otherwise "-unknown-DejaVu Sans Mono-bold-normal-normal-*-14-*-*-*-m-0-iso10646-1"))))
     (set-face-attribute 'default nil :font font)))
