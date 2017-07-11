4bb8c0d61b4d976efd066ca3dc1e00878eef0ecc
Dave Benjamin <dave@ramenlabs.com>
2017-07-10 21:26:02 -0700

    Tweaks for JSX support

--- personal/init-javascript.el
+++ personal/init-javascript.el
@@ -5,8 +5,15 @@
             (esk-prog-mode-hook)
             (define-key js2-mode-map (kbd "RET") nil)))
 
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (esk-prog-mode-hook)
+            (define-key rjsx-mode-map (kbd "<") nil)
+            (define-key rjsx-mode-map (kbd "C-c /") 'sgml-close-tag)))
+
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (setq js2-enter-indents-newline t)
 (setq js2-mirror-mode nil)
+(setq js2-strict-trailing-comma-warning nil)
