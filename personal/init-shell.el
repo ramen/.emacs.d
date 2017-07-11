193150e5f2d8c1e5b16775d9fa017b3eaa606cb1
Dave Benjamin <dave@ramenlabs.com>
2017-07-10 21:26:18 -0700

    Add keyboard shortcut for pasting in ansi-term

--- personal/init-shell.el
+++ personal/init-shell.el
@@ -1,3 +1,7 @@
 ;;; init-shell.el --- shell mode
 
 (setq auto-mode-alist (cons '("\\.zsh$" . sh-mode) auto-mode-alist))
+
+;; http://oremacs.com/2015/01/01/three-ansi-term-tips/
+(eval-after-load "term"
+  '(define-key term-raw-map (kbd "C-c C-y") 'term-paste))
