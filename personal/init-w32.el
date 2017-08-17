ce74704248d5ecb76eb87c75f3447350b3a960a2
Dave Benjamin <dave@ramenlabs.com>
2017-08-17 06:54:08 -0700

    Make rgrep work on windows

--- personal/init-w32.el
+++ personal/init-w32.el
@@ -4,6 +4,7 @@
   (unbold-all-faces)
   (add-hook 'after-change-major-mode-hook 'unbold-all-faces)
   (setq find-program "gfind")
+  (setq grep-find-template "gfind . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null ;")
   (setq w32-pass-lwindow-to-system nil
         w32-pass-rwindow-to-system nil
         w32-lwindow-modifier 'super
