90f5d2997b4d7d52fd9d7ca7db16a6fc509935c0
Dave Benjamin <dave@teamaud.io>
2023-05-27 10:22:41 -0700

    Change default font due to 4k monitor

--- personal/init-fonts.el
+++ personal/init-fonts.el
@@ -3,7 +3,7 @@
 (when window-system
   (let ((font
          (case window-system
-           (w32 "-outline-Consolas-normal-normal-normal-*-14-*-*-*-c-*-iso10646-1")
+           (w32 "-outline-Consolas-normal-normal-normal-*-27-*-*-*-c-*-iso10646-1")
            (ns "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
            (otherwise "-DAMA-Ubuntu Mono-normal-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
     (set-face-attribute 'default nil :font font)))
