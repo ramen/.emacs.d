ce9616e2ad5236dfd4f169d3ecac197a62b3cb68
Dave Benjamin <dave@teamaud.io>
2023-05-27 10:22:28 -0700

    Windows tweaks

--- personal/init-w32.el
+++ personal/init-w32.el
@@ -6,9 +6,10 @@
   (setq explicit-shell-file-name "C:/Windows/System32/bash.exe")
   (setq find-ls-option '("-exec ls -ld {} \\;" . "-ld"))
   (setq find-program "wsl find")
+  (setq frame-title-format "%b")
   (setq grep-find-template "wsl find . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null \\;")
-  (setq w32-pass-lwindow-to-system nil
-        w32-pass-rwindow-to-system nil
+  (setq ;; w32-pass-lwindow-to-system nil
+        ;; w32-pass-rwindow-to-system nil
         w32-lwindow-modifier 'super
         w32-rwindow-modifier 'super
         w32-apps-modifier 'hyper))
