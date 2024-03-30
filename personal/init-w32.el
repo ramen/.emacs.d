76a653d2a7bb40fdb434063c5b4640550685313b
Dave Benjamin <dave@teamaud.io>
2024-03-30 08:12:17 -0700

    Windows tweaks

--- personal/init-w32.el
+++ personal/init-w32.el
@@ -3,9 +3,9 @@
 (when (eq window-system 'w32)
   (unbold-all-faces)
   (add-hook 'after-change-major-mode-hook 'unbold-all-faces)
-  (setq explicit-shell-file-name "C:/Windows/System32/bash.exe")
   (setq find-ls-option '("-exec ls -ld {} \\;" . "-ld"))
   (setq find-program "wsl find")
+  (setq diff-command "wsl diff")
   (setq frame-title-format "%b")
   (setq grep-find-template "wsl find . <X> -type f <F> -exec grep <C> -nHP -e <R> {} /dev/null \\;")
   (setq ;; w32-pass-lwindow-to-system nil
